b69ee3d6c56d210b366af56a5e9685160614b759
ExposureMG <exposuremg@protonmail.com>
2026-05-28 21:53:06 +0100

    style: remove build_release script

--- build_release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cargo build --release --no-default-features --features cli
-
-git clone https://github.com/ExposureMG/gxBuild-Support-Files.git release
-
-cp target/release/gxbuild release/
